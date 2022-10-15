cd1318963df12c5dc53db137c47eee3a35142867
Raluca Streja <raluca.streja@gmail.com>
2022-10-15 12:36:19 +0300

    commented out the model validation

---
+++ Migrations/20221015091132_DebugPublisher.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streja_Raluca_Lab2.Migrations
+{
+    public partial class DebugPublisher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -21,8 +21,7 @@ namespace Streja_Raluca_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
-            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
-"PublisherName");
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
             return Page();
         }
 
@@ -33,11 +32,11 @@ namespace Streja_Raluca_Lab2.Pages.Books
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+        /*  if (!ModelState.IsValid)
             {
                 return Page();
             }
-
+        */
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
 
