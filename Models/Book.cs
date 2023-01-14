484f9e56f852c8e20bf23a5620c1b88fa47ae0b7
Raluca Streja <raluca.streja@gmail.com>
2023-01-14 20:05:20 +0200

    Updated the name of two fields

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Policy;
 
 namespace Streja_Raluca_Lab2.Models
 {
@@ -15,6 +14,7 @@ namespace Streja_Raluca_Lab2.Models
         [Column(TypeName = "decimal(6,2)")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Publishing Date")]
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
         public int PublisherID { get; set; }
--- Models/Publisher.cs
+++ Models/Publisher.cs
@@ -1,8 +1,13 @@
-﻿namespace Streja_Raluca_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Streja_Raluca_Lab2.Models
 {
     public class Publisher
     {
         public int ID { get; set; }
+
+        [Display(Name = "Publisher Name")]
         public string PublisherName { get; set; }
         public ICollection<Book>? Books { get; set; }
     }
