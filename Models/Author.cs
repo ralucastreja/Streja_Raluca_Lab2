b1285a17886397bc9df080f18f2a9239a13753a0
Raluca Streja <raluca.streja@gmail.com>
2025-01-28 22:42:01 +0200

    punctul 2

---
+++ Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Streja_Raluca_Lab2.Models
+    {
+    public class Author
+        {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        }
+    }
--- Models/Book.cs
+++ Models/Book.cs
@@ -10,7 +10,6 @@ namespace Streja_Raluca_Lab2.Models
 
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        public string Author { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
@@ -21,5 +20,9 @@ namespace Streja_Raluca_Lab2.Models
         public int? PublisherID { get; set; }
 
         public Publisher? Publisher { get; set; } // navigation property
+
+        public int? AuthorID { get; set; }
+
+        public Author? Author { get; set; } // navigation property
         }
     }
